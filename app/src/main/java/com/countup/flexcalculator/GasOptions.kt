39a8f4233f9f1629d2c7a8cb6d6ff5c7dc07d340
leonardo antunes <brombilladeveloper@gmail.com>
2025-03-24 08:31:40 -0300

    Parameters from gasListScreen returning

--- app/src/main/java/com/countup/flexcalculator/GasOptions.kt
+++ app/src/main/java/com/countup/flexcalculator/GasOptions.kt
@@ -16,22 +16,30 @@ class GasOptions : AppCompatActivity() {
         binding = ActivityGasOptionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Pego do array de valores as options
         val fuelTypes = resources.getStringArray(R.array.fuel_types)
 
+
+        //Esse adapter vai retornar uma view, pra cada elemento na lista
         val adapter = ArrayAdapter(
-            this, android.R.layout.simple_list_item_1,
+            this,
+            android.R.layout.simple_list_item_1,
             fuelTypes
         )
 
         binding.listViewFuelOptions.adapter = adapter
 
+        //aqui simplesmente pegamos a position e o valor e quando selecionamos
+        //ele volta a tela
         binding.listViewFuelOptions.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
             val selectedFuel = fuelTypes[position]
 
             val resultIntent = Intent()
+            //adiciona o valor
             resultIntent.putExtra("SELECTED_FUEL", selectedFuel)
             setResult(RESULT_OK, resultIntent)
 
+            //fecha a activity
             finish()
         }
     }
--- app/src/main/java/com/countup/flexcalculator/MainActivity.kt
+++ app/src/main/java/com/countup/flexcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.countup.flexcalculator
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,20 +13,59 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val REQUEST_FUEL_TYPE_1 = 100
+    private val REQUEST_FUEL_TYPE_2 = 101
+
+    private val fuelLauncher1 = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        result -> if(result.resultCode == RESULT_OK) {
+            val selectedFuel = result.data?.getStringExtra("SELECTED_FUEL")
+            binding.editTextConsumption1.setText(selectedFuel)
+        }
+    }
+
+    private val fuelLauncher2 = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+            result -> if(result.resultCode == RESULT_OK) {
+            val selectedFuel = result.data?.getStringExtra("SELECTED_FUEL")
+            binding.editTextConsumption.setText(selectedFuel)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonConsumption1.setOnClickListener{
+        binding.buttonConsumption1.setOnClickListener {
             val intent = Intent(this, GasOptions::class.java)
-            startActivity(intent)
+            fuelLauncher1.launch(intent)
         }
 
-        binding.buttonConsumption2.setOnClickListener{
+        binding.buttonConsumption2.setOnClickListener {
             val intent = Intent(this, GasOptions::class.java)
-            startActivity(intent)
+            fuelLauncher2.launch(intent)
+        }
+ }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            val selectedFuel = data?.getStringExtra("SELECTED_FUEL")
+
+            when(requestCode) {
+                REQUEST_FUEL_TYPE_1 -> {
+                    binding.buttonConsumption1.text = selectedFuel
+                }
+
+                REQUEST_FUEL_TYPE_2 -> {
+                    binding.buttonConsumption2.text = selectedFuel
+                }
+            }
+
         }
     }
-
 }
